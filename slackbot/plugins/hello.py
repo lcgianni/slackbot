05f35ba0758aec5af2a873b0b97761ec3424dbb6
Luigi Gianni <lcgianni@gmail.com>
2017-08-23 17:39:47 -0700

    more stuff

--- slackbot/plugins/hello.py
+++ slackbot/plugins/hello.py
@@ -1,17 +1,15 @@
 #coding: UTF-8
 import re
-#import random
+import random
 
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 
-"""
 def choose_restaurant(meal='lunch'):
     address = {'dinner': {'4505 BBQ': '234 Divisadero St.',
                           'Signores Pizza': '234 Fulton St.'}}
     choice = random.choice(address[meal].keys())
     return choice, address[meal][choice]
-"""
 
 @respond_to('hello|hi|hey|olá|ola|oi', re.IGNORECASE)
 def hello_reply(message):
@@ -19,8 +17,7 @@ def hello_reply(message):
 
 @respond_to('aeroporto.*casa', re.IGNORECASE)
 def hello_reply(message):
-    message.reply('A melhor forma de ir do aeroporto até a casa é pedindo um Uber. Qualquer dúvida,
-                   me pergunte qual o endereço da casa.')
+    message.reply('A melhor forma de ir do aeroporto até a casa é pedindo um Uber. Qualquer dúvida, me pergunte qual o endereço da casa.')
 
 @respond_to('endereço|endereco|endreco.*casa|house', re.IGNORECASE)
 def hello_reply(message):
@@ -28,8 +25,7 @@ def hello_reply(message):
 
 @respond_to('entrar.*casa', re.IGNORECASE)
 def hello_reply(message):
-    message.reply('Do lado esquerdo da porta, você vai encontrar um cofre. A senha é 143133.
-                   Dentro deste cofre, você encontrará a chave da casa.')
+    message.reply('Do lado esquerdo da porta, você vai encontrar um cofre. A senha é 143133. Dentro deste cofre, você encontrará a chave da casa.')
 
 @respond_to('endereço|endereco|endreco.*galvanize|coworking', re.IGNORECASE)
 def hello_reply(message):
