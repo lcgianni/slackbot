3d29711c7a0c8f557a0c3041a3324d9bc6151974
Luigi Gianni <lcgianni@gmail.com>
2017-08-23 17:46:47 -0700

    more stuff

--- slackbot/plugins/hello.py
+++ slackbot/plugins/hello.py
@@ -47,27 +47,3 @@ def hello_reply(message):
 @respond_to('musica|música', re.IGNORECASE)
 def hello_reply(message):
     message.reply('Miley Cyrus')
-
-@respond_to('^reply_webapi$')
-def hello_webapi(message):
-    message.reply_webapi('hello there!', attachments=[{
-        'fallback': 'test attachment',
-        'fields': [
-            {
-                'title': 'test table field',
-                'value': 'test table value',
-                'short': True
-            }
-        ]
-    }])
-
-
-@respond_to('^reply_webapi_not_as_user$')
-def hello_webapi_not_as_user(message):
-    message.reply_webapi('hi!', as_user=False)
-
-
-@respond_to('hello_formatting')
-def hello_reply_formatting(message):
-    # Format message with italic style
-    message.reply('_hello_ sender!')
