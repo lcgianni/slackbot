0684ca777c3fb0c05116d51d49f45c13716f3855
Luigi Gianni <lcgianni@gmail.com>
2017-08-23 17:35:18 -0700

    more stuff

--- slackbot/plugins/hello.py
+++ slackbot/plugins/hello.py
@@ -1,24 +1,38 @@
 #coding: UTF-8
 import re
+import random
+
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
+
+def choose_restaurant(meal='lunch'):
+    address = {'dinner': {'4505 BBQ': '234 Divisadero St.',
+                          'Signores Pizza': '234 Fulton St.'}}
+    choice = random.choice(address[meal].keys())
+    return choice, address[meal][choice]
 
 
 @respond_to('hello|hi|hey|olá|ola|oi', re.IGNORECASE)
 def hello_reply(message):
     message.reply('Olá, como posso lhe ajudar?')
 
-@respond_to('entrar.*casa', re.IGNORECASE)
+@respond_to('aeroporto.*casa', re.IGNORECASE)
 def hello_reply(message):
-    message.reply('Do lado esquerdo da porta, você vai encontrar um cofre.')
+    message.reply('A melhor forma de ir do aeroporto até a casa é pedindo um Uber. Qualquer dúvida,
+                   me pergunte qual o endereço da casa.')
 
 @respond_to('endereço|endereco|endreco.*casa|house', re.IGNORECASE)
 def hello_reply(message):
-    message.reply('Esse é o endereço da casa.')
+    message.reply('O endereço da casa é 1314 Fulton St.')
+
+@respond_to('entrar.*casa', re.IGNORECASE)
+def hello_reply(message):
+    message.reply('Do lado esquerdo da porta, você vai encontrar um cofre. A senha é 143133.
+                   Dentro deste cofre, você encontrará a chave da casa.')
 
 @respond_to('endereço|endereco|endreco.*galvanize|coworking', re.IGNORECASE)
 def hello_reply(message):
-    message.reply('Esse é o endereço da Galvanize.')
+    message.reply('O endereço da Galvanize é 543 Howard St.')
 
 @respond_to('almoço|almoco|almocar', re.IGNORECASE)
 def hello_reply(message):
@@ -26,7 +40,12 @@ def hello_reply(message):
 
 @respond_to('janta|jantar', re.IGNORECASE)
 def hello_reply(message):
-    message.reply('Que tal jantar neste lugar?')
+    choice, address = choose_restaurant(meal='dinner')
+    message.reply("Que tal jantar no %s? O endereço é %s", (choice, address))
+
+@respond_to('evento', re.IGNORECASE)
+def hello_reply(message):
+    message.reply('Dê uma olhada no eventbrite.com ou meetup.com')
 
 @respond_to('musica|música', re.IGNORECASE)
 def hello_reply(message):
